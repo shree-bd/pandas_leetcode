7f3f897c92a410b25c2cb18c83ea93d8a0c439b1
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-03-07 12:26:36 -0500

    Time: 915 ms (25.82%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select  p.firstName,p.lastName,ifnull(a.city,null) as city,ifnull(a.state,null) as state       
+select  p.firstName,p.lastName,city,state       
 from person p left join address a on p.personId=a.personId 
