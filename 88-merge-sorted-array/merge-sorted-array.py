842fe28519fce2b9737ad58bc921373f2d5d6fa5
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-03-07 23:55:29 -0500

    Time: 42 ms (43.14%) | Memory: 16.7 MB (40.95%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # last index nums1
+        i=m+n-1
+
+        # merge in reverse order
+        while m > 0 and n>0:
+            if nums1[m-1]>nums2[n-1]:
+                nums1[i]=nums1[m-1]
+                m -=1
+            else:
+                nums1[i]=nums2[n-1]
+                n -=1
+
+            i=i-1
+            
+        # cover all the edge cases
+        while n>0:
+            nums1[i]=nums2[n-1]
+            n-=1
+            i-=1
+       
+        
